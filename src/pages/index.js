1146c92142e02836a3dd82032c1bc3269aa1a2e5
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-04-16 20:26:49 +0300

    Adaugă formularul pe site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,128 @@
-import React, { useState } from "react";
+import React from "react";
+import { Helmet } from "react-helmet";
+import { useForm, Controller } from "react-hook-form";
+import Select from "react-select";
 
-function App() {
-  const [initialSalary, setInitialSalary] = useState(0);
+import salariesPerCounty from "../data/salariesPerCounty.json";
+import salariesPerJob from "../data/salariesPerJob.json";
 
-  function changeSalary(event) {
-    setInitialSalary(event.target.value);
-  }
-
-  let salary = initialSalary;
-  let newSalary = (initialSalary * 75) / 100;
-  if (4072 < newSalary) {
-    newSalary = 4072;
-  }
-
+export default function App() {
   return (
-    <div className="bigdiv">
-      <div className="title">
-        <h1>Somaj Tehnic</h1>
-      </div>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Șomajul tehnic în perioada stării de urgență</title>
+      </Helmet>
 
-      <div className="information1">
-        <p>Seek and Destroy better than Seek and Love</p>
-      </div>
+      <h1>Șomaj Tehnic</h1>
 
-      <form>
-        <label>
-          Ce salariu net aveai inainte de instaurarea starii de urgenta? <br />
-          <input
-            className="inputRon"
-            type="text"
-            name="RON"
-            value={initialSalary}
-            onChange={changeSalary}
-          />
-        </label>
+      <p>
+        Dacă ești una dintre persoanele care a intrat în șomaj tehnic din cauza
+        epidemiei de CoVID-19, suntem aici să te ajutăm.
+      </p>
 
-        <span>RON</span>
-
-        <p>Salariu net initial: {salary} RON</p>
-        <p>Salariu net actual: {newSalary} RON</p>
-      </form>
-    </div>
+      <SalaryForm />
+    </>
   );
 }
 
-export default () => <App />;
+const SalaryForm = () => {
+  const {
+    control,
+    formState: { isSubmitting },
+    register,
+    handleSubmit,
+    watch,
+    errors,
+    setError,
+  } = useForm({
+    mode: "onChange",
+  });
+
+  const initialSalary = watch("initialSalary");
+  const onSubmit = async (data) => {
+    try {
+      // fetch
+    } catch (error) {
+      setError("submit", "submitError", `Doh! ${error.message}`);
+    }
+  };
+
+  const showSalaryReport = !errors.initialSalary && initialSalary;
+
+  let invalidSalaryWarning = null;
+  if (errors.initialSalary) {
+    invalidSalaryWarning = <div>Salariu invalid</div>;
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <p>
+          Ce salariu <u>brut</u> aveai înainte de instaurea stării de urgență?
+        </p>
+        <div>
+          <input
+            disabled={isSubmitting}
+            type="text"
+            id="initialSalary"
+            name="initialSalary"
+            placeholder="2000"
+            ref={register({ pattern: /^[0-9]+$/i })}
+          />
+          <label htmlFor="initialSalary">RON</label>
+          {invalidSalaryWarning}
+        </div>
+      </div>
+      {showSalaryReport && (
+        <SalaryReport initialSalary={initialSalary} control={control} />
+      )}
+      <p>
+        Cu permisiunea ta, strângem date anonime de la oameni afectați de
+        pandemie din toată țara, și raportăm aceste date autorităților.
+      </p>
+      <input disabled={isSubmitting} type="submit" value="Trimite dat " />
+    </form>
+  );
+};
+
+const SalaryReport = ({ initialSalary, control }) => {
+  // TODO: use per job type salary caps
+  const newSalary = Math.min(initialSalary * 0.75, 4072);
+
+  const countyNames = Object.keys(salariesPerCounty);
+
+  const countyOptions = countyNames.map((name) => ({
+    label: name,
+    value: name,
+  }));
+
+  const jobNames = Object.keys(salariesPerJob);
+
+  const jobOptions = jobNames.map((name) => ({
+    label: name,
+    value: name,
+  }));
+
+  return (
+    <>
+      <div>Salariu brut inițial: {initialSalary} RON</div>
+      <div>Salariu brut actual: {newSalary} RON</div>
+
+      <Controller
+        as={Select}
+        control={control}
+        name="county"
+        placeholder="Alegeți județul"
+        options={countyOptions}
+      />
+      <Controller
+        as={Select}
+        control={control}
+        name="job"
+        placeholder="Alegeți domeniul de activitate"
+        options={jobOptions}
+      />
+    </>
+  );
+};
