b0bc96a9dab2ab56df74378176905e82a62a94a7
Bogdan Nitica <bogdan.nitica99@gmail.com>
2020-04-16 17:41:30 +0300

    switch to hooks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { salariuInitial: 0 };
+function App() {
+  const [initialSalary, setInitialSalary] = useState(0);
+
+  function changeSalary(event) {
+    setInitialSalary(event.target.value);
   }
 
-  changeSalary(event) {
-    this.setState({
-      salariuInitial: event.target.value,
-    });
+  let salary = initialSalary;
+  let newSalary = (initialSalary * 75) / 100;
+  if (4072 < newSalary) {
+    newSalary = 4072;
   }
 
-  render() {
-    let salary = this.state.salariuInitial;
-    let newSalary = (this.state.salariuInitial * 75) / 100;
-    if (4072 < newSalary) {
-      newSalary = 4072;
-    }
-
-    return (
-      <div className="bigdiv">
-        <div className="title">
-          <h1>Somaj Tehnic</h1>
-        </div>
-
-        <div className="information1">
-          <p>Seek and Destroy better than Seek and Love</p>
-        </div>
-
-        <form>
-          <label>
-            Ce salariu net aveai inainte de instaurarea starii de urgenta?{" "}
-            <br />
-            <input
-              className="inputRon"
-              type="text"
-              name="RON"
-              value={this.state.salariuInitial}
-              onChange={this.changeSalary.bind(this)}
-            />
-          </label>
-
-          <span>RON</span>
-
-          <p>Salariu net initial: {salary} RON</p>
-          <p>Salariu net actual: {newSalary} RON</p>
-        </form>
+  return (
+    <div className="bigdiv">
+      <div className="title">
+        <h1>Somaj Tehnic</h1>
       </div>
-    );
-  }
+
+      <div className="information1">
+        <p>Seek and Destroy better than Seek and Love</p>
+      </div>
+
+      <form>
+        <label>
+          Ce salariu net aveai inainte de instaurarea starii de urgenta? <br />
+          <input
+            className="inputRon"
+            type="text"
+            name="RON"
+            value={initialSalary}
+            onChange={changeSalary}
+          />
+        </label>
+
+        <span>RON</span>
+
+        <p>Salariu net initial: {salary} RON</p>
+        <p>Salariu net actual: {newSalary} RON</p>
+      </form>
+    </div>
+  );
 }
 
 export default () => <App />;
